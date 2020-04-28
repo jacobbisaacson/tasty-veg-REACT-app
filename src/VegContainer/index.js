3ed3397ca4975b7d87546ee7fbad7bcad016e06e
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-28 09:09:17 -0500

    issue with fetching url

--- src/VegContainer/index.js
+++ src/VegContainer/index.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 
 export default class VegContainer extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			vegs: []
+		}
+	}
+
+  componentDidMount() {
+    this.getVegs()
+  }
+
+  getVegs = async () => {
+    try {
+      const url = process.env.REACT_APP_API_URL + "/api/v1/vegs/"
+      const vegsResponse = await fetch(url)
+      const vegsJson = await vegsResponse.json()
+      console.log(vegsJson);
+    } catch(err) {
+      console.error("error getting veg data", err);
+    }
+  }
+
 	render() {
 		return(
 			<h2>Veg Container</h2>
